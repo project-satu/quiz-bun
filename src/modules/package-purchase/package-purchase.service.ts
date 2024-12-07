0cd4700846917a98de549de39e21fa86bc43ad18
awahids <awahid.safhadi@gmail.com>
2024-12-07 23:00:32 +0700

    Refactor package purchase service to include unique amount generation and transaction status check

--- src/modules/package-purchase/package-purchase.service.ts
+++ src/modules/package-purchase/package-purchase.service.ts
@@ -4,7 +4,7 @@ import { CreatePackagePurchaseDto } from './dto/package-purchase.dto';
 import { ModulePackageService } from '../module-package/module-package.service';
 import { errorResponse } from '@/utils/helpers/response.helper';
 import { TransactionStatusValue } from '@/constant/enum/transaction-status.enum';
-import { GenerateTrxId } from '@/utils/generateCode.util';
+import { GenerateTrxId, GenerateUniqueAmount } from '@/utils/generateCode.util';
 import { Filter } from '@/common/dto.common';
 import { paginationResponse, paramPaginate } from '@/utils/helpers/pagination.helper';
 
@@ -79,16 +79,19 @@ export class PackagePurchaseService {
     const foundModulePackage = await this.modulePackageService.findOne(
       modulePackage.uuid,
     );
+    if (!foundModulePackage) {
+      return errorResponse('Module package not found');
+    }
 
-    if (!foundModulePackage) return errorResponse('module package not found');
 
     const trxStatus = await this.prisma.transactionStatus.findFirst({
       where: {
         value: TransactionStatusValue.TRX_PENDING,
       },
     });
-
-    if (!trxStatus) return errorResponse('transaction status not found', 403);
+    if (!trxStatus) {
+      return errorResponse('Transaction status not found', 403);
+    }
 
     const currentDate = new Date();
     const expirationDate = new Date(
@@ -97,23 +100,108 @@ export class PackagePurchaseService {
       ),
     );
 
-    const packagePurchase = await this.prisma.packagePurchase.create({
-      data: {
+    const checkPurchasePending = await this.prisma.packagePurchase.findFirst({
+      where: {
         userId: user.id,
-        packageId: foundModulePackage?.id,
-        isActive: false,
-        expiredAt: expirationDate,
+        packageId: foundModulePackage.id,
+        status: {
+          value: {
+            in: [TransactionStatusValue.TRX_PENDING],
+          },
+        },
         transactions: {
-          create: {
-            amount: foundModulePackage.price,
-            purchaseAt: null,
-            statusId: trxStatus.id,
-            transactionId: GenerateTrxId(user.id, foundModulePackage.id),
+          some: {
+            status: {
+              value: {
+                in: [TransactionStatusValue.TRX_PENDING],
+              },
+            },
           },
         },
       },
     });
 
-    return packagePurchase;
+    if (checkPurchasePending) {
+      return errorResponse(
+        `${foundModulePackage.title} is still waiting for payment.`,
+        403,
+      );
+    }
+
+    const uniqueCode = GenerateUniqueAmount(foundModulePackage.price);
+    // Start a transaction
+    const result = await this.prisma.$transaction(async (prisma) => {
+      const packagePurchase = await prisma.packagePurchase.create({
+        data: {
+          userId: user.id,
+          packageId: foundModulePackage.id,
+          isActive: false,
+          expiredAt: expirationDate,
+          statusId: trxStatus.id,
+          transactions: {
+            create: {
+              amount: foundModulePackage.price + uniqueCode,
+              purchaseAt: null,
+              statusId: trxStatus.id,
+              transactionId: GenerateTrxId(user.id, foundModulePackage.id),
+            },
+          },
+        },
+        select: {
+          id: true,
+          uuid: true,
+          isActive: true,
+          expiredAt: true,
+          status: {
+            select: {
+              id: true,
+              uuid: true,
+              title: true,
+              value: true,
+            },
+          },
+          package: {
+            select: {
+              id: true,
+              uuid: true,
+              title: true,
+              description: true,
+              price: true,
+              durationInMonth: true,
+            },
+          },
+          transactions: {
+            select: {
+              id: true,
+              uuid: true,
+              amount: true,
+              purchaseAt: true,
+              transactionId: true,
+              status: {
+                select: {
+                  id: true,
+                  uuid: true,
+                  title: true,
+                  value: true,
+                },
+              },
+            },
+          },
+          user: {
+            select: {
+              id: true,
+              uuid: true,
+              name: true,
+              email: true,
+            },
+          },
+        },
+      });
+
+      return packagePurchase;
+    });
+
+    return result;
   }
+
 }
--- src/utils/generateCode.util.ts
+++ src/utils/generateCode.util.ts
@@ -2,8 +2,15 @@ import * as dayjs from 'dayjs';
 
 export function GenerateTrxId(userId: number, packageId: number): string {
   const now = dayjs().format('DDMM');
-  const random = Math.floor(Math.random() * 1000000);
-  const code = `${userId}${packageId}-${now}${random}`;
+  const stringRandom = Math.random().toString(36).slice(2).toLocaleUpperCase();
+  const code = `${stringRandom}${userId}-${packageId}${now}`;
 
   return code;
+}
+
+export function GenerateUniqueAmount(amount: number): number {
+  const maxUniqueCode = amount <= 100000 ? 99 : 999;
+  const uniqueCode = Math.floor(Math.random() * maxUniqueCode) + 1;
+
+  return uniqueCode;
 }
