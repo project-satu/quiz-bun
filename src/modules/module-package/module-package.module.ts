3f8e085844d176fd07456915ef6468960f082586
awahids <awahid.safhadi@gmail.com>
2024-12-08 10:48:34 +0700

    Added new endpoint to retrieve quizzes by module package

--- src/modules/module-package/module-package.module.ts
+++ src/modules/module-package/module-package.module.ts
@@ -4,9 +4,16 @@ import { ModulePackageService } from './module-package.service';
 import { ModulePackageController } from './module-package.controller';
 import { PackagePurchaseService } from '../package-purchase/package-purchase.service';
 import { MaterialService } from '../material/material.service';
+import { QuizService } from '../quiz/quiz.service';
 
 @Module({
-  providers: [ModulePackageService, PrismaService, PackagePurchaseService, MaterialService],
+  providers: [
+    ModulePackageService,
+    PrismaService,
+    PackagePurchaseService,
+    MaterialService,
+    QuizService
+  ],
   controllers: [ModulePackageController],
   exports: [ModulePackageService],
 })
